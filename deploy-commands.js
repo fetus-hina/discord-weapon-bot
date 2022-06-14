bf710c3a661d19677862fde12c3489e405a43fff
AIZAWA Hina <hina@fetus.jp>
2022-06-14 18:36:16 +0900

    weapon{1,2} の category 引数に候補値を追加

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,18 +1,63 @@
+import WEAPON1 from './weapons/splatoon1.js';
+import WEAPON2 from './weapons/splatoon2.js';
+import getCategoryList from './converters/get-types.js';
 import { CLIENT_ID, GUILD_ID, TOKEN } from './config.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import { SlashCommandBuilder } from '@discordjs/builders';
+
+const categoryNames = {
+  blaster: 'blaster - ブラスター',
+  brella: 'brella - シェルター(カサ)',
+  brush: 'brush - フデ',
+  charger: 'charger - チャージャー',
+  maneuver: 'maneuver - マニューバー',
+  reelgun: 'reelgun - リールガン',
+  roller: 'roller - ローラー',
+  shooter: 'shooter - シューター(他に該当するものを除く)',
+  slosher: 'slosher - スロッシャー(バケツ)',
+  spinner: 'spinner - スピナー'
+};
+
+const addWeaponCategoryEnum = (option, categoryList) => {
+  // addChoices は次のように:
+  //    option.addChoices(
+  //      { name: 'Funny', value: 'gif_funny' },
+  //      { name: 'Meme', value: 'gif_meme' },
+  //      { name: 'Movie', value: 'gif_movie' },
+  //    ));
+  // 配列ではなく、引数にオブジェクトを連ねて渡す必要があるので
+  // function.apply を使って頑張る必要がある
+  option.addChoices.apply(
+    option,
+    categoryList.map(id => ({
+      name: categoryNames[id] ?? id,
+      value: id
+    }))
+  );
+  return option;
+};
 
 const commands =
   [
     new SlashCommandBuilder()
       .setName('weapon1')
       .setDescription('Splatoon 1 のブキルーレット')
-      .addStringOption(opt => opt.setName('category').setDescription('ブキ種別 (`/category1` で取得)')),
+      .addStringOption(
+        opt => addWeaponCategoryEnum(
+          opt.setName('category').setDescription('ブキ種別 (`/category1` で取得)'),
+          getCategoryList(WEAPON1)
+        )
+      ),
     new SlashCommandBuilder()
       .setName('weapon2')
       .setDescription('Splatoon 2 のブキルーレット')
-      .addStringOption(opt => opt.setName('category').setDescription('ブキ種別 (`/category2` で取得)')),
+      .addStringOption(
+        opt => addWeaponCategoryEnum(
+          opt.setName('category').setDescription('ブキ種別 (`/category2` で取得)'),
+          getCategoryList(WEAPON2)
+        )
+      ),
     new SlashCommandBuilder().setName('category1').setDescription('Splatoon 1 のブキ種別リスト'),
     new SlashCommandBuilder().setName('category2').setDescription('Splatoon 2 のブキ種別リスト')
   ].map(command => command.toJSON());
