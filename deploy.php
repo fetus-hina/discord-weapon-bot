7f4d8b96796ce47ce2626b601eacb355f955d402
AIZAWA Hina <hina@fetus.jp>
2023-09-28 21:09:23 +0900

    Update discord-api-types

--- deploy.php
+++ deploy.php
@@ -14,8 +14,10 @@ set('shared_dirs', []);
 set('writable_dirs', []);
 set('writable_mode', 'chmod');
 set('writable_chmod_recursive', false);
+set('github_keys', ['github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl']);
 
-host('ayanami2')
+host('ayanami3')
+    ->hostname('2403:3a00:202:1127:49:212:205:127')
     ->user('discord-bot')
     ->stage('production')
     ->roles('app')
@@ -24,6 +26,7 @@ host('ayanami2')
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
+    'deploy:ssh_config',
     'deploy:git_config',
     'deploy:lock',
     'deploy:release',
@@ -41,8 +44,31 @@ task('deploy', [
 
 after('deploy:failed', 'deploy:unlock');
 
+task('deploy:ssh_config', function () {
+    run(sprintf('mkdir -p --mode=%s %s', '700', '~/.ssh'));
+    run('touch ~/.ssh/known_hosts');
+    run('chmod 600 ~/.ssh/known_hosts');
+    run(sprintf('ssh-keygen -R %s', escapeshellarg('github.com')));
+    foreach (get('github_keys') as $line) {
+        run(
+            vsprintf('echo %s >> ~/.ssh/known_hosts', [
+                escapeshellarg($line),
+            ]),
+        );
+    }
+    run('ssh-keygen -H');
+    run('rm -f ~/.ssh/known_hosts.old');
+});
+
 task('deploy:git_config', function () {
     run('git config --global advice.detachedHead false');
+    run(
+        vsprintf('git config --global core.sshCommand %s', [
+            escapeshellarg(
+                'ssh -o HostKeyAlgorithms=ssh-ed25519 -o KexAlgorithms=curve25519-sha256,curve25519-sha256@libssh.org',
+            ),
+        ]),
+    );
 });
 
 task('deploy:shared', function () {
